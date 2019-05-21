fe276b4c88d75b3262c675606f0e347ae4d9050b
Devin Arnold <dbarnold321@gmail.com>
2019-05-21 15:49:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,14 @@ def key_for_min_value(hash)
    # min_key = key if min_key == nil 
     #min_value = value if min_value == nil
     #if value < min_value
-    	min_value = value
-    	min_key = key
-    end
+    #	min_value = value
+    #	min_key = key
+  #  end
+  min_k = nil
+  min_V = nil
+  hash.each do |key, value|
+    min_k = key if min_k == nil
+    min_v = value if min_v == nil
   end
   min_key
 end
